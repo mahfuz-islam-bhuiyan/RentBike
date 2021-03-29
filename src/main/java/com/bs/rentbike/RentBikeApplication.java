64faf1af6ab156333826395db251a27db2e0b80d
mahfuz-islam-bhuiyan <mahfuz.bhuiyan@brainstation23.com>
2021-03-29 12:27:34 +0600

    initial commit

---
+++ src/main/java/com/bs/rentbike/RentBikeApplication.java
@@ -0,0 +1,13 @@
+package com.bs.rentbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bs/rentbike/RentBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.rentbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
